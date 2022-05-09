a4ef992fb6826b22386f371785be855c1dc6e83d
maureen <maureen.murimi@moringaschool.com>
2022-05-09 20:36:44 +0300

    add arrow functions

---
+++ index.js
@@ -0,0 +1,9 @@
+function divide(){
+    return 2000 / 100
+
+}
+// const twoAdder = (x) => x + 2;
+// const add = (parameter1, parameter2) => parameter1 + parameter2;
+const square = (num) => num * num;
+
+const add = (num1, num2) => num1 + num2;
